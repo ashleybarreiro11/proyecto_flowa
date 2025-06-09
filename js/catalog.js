3ba61e38e96975af5ccefc927c69cb76168aaf10
Josemsda <you@example.com>
2025-06-08 22:36:54 -0500

    Search bar funcionando

--- js/catalog.js
+++ js/catalog.js
@@ -1,10 +1,16 @@
+const inputsearchBar = document.getElementById("search-bar")
 async function food (){
     const Foodget = await fetch('https://dummyjson.com/recipes')
     const jsonfood = await Foodget.json()
     console.log(jsonfood); 
     return jsonfood
 }
-
+async function foodSearch (){
+    const searchGet = await fetch(`https://dummyjson.com/recipes/search?q=${inputsearchBar.value}`)
+    const parsefood = await searchGet.json()
+    console.log(parsefood)
+    return parsefood
+}
 const data = food()
 console.log(data);
 
@@ -45,3 +51,28 @@ const localUser = localStorage.getItem("logged-user")
 const localUserJson = JSON.parse(localUser)
 console.log(localUserJson.favorites,id); 
 }
+
+
+async function searchBar() {
+    recipesContainer.innerHTML = ""
+    const dataSearch = await foodSearch()
+    dataSearch.recipes.forEach(item => {
+        const newRecipe = new recipecard(
+            item.id,
+            item.name,
+            item.image,
+            item.ingredients,
+            item.instructions,
+            item.prepTimeMinutes,
+            item.cookTimeMinutes,
+            item.caloriesPerServing,
+            item.servings,
+            item.difficulty,
+            item.cuisine,
+            item.tags
+        );
+        recipesContainer.innerHTML += newRecipe.card();
+    });
+}
+
+inputsearchBar.addEventListener("keypress", searchBar())
